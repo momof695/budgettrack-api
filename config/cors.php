04310d7b7899fe9c9e0d34d97ecac81874dff5c9
Mohamed <sawadogom816@gmail.com>
2026-05-09 14:53:13 +0000

    fix CORS for production

--- config/cors.php
+++ config/cors.php
@@ -3,8 +3,11 @@
 return [
     'paths'                    => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods'          => ['*'],
-    'allowed_origins'          => ['http://localhost:5173'],
-    'allowed_origins_patterns' => [],
+    'allowed_origins'          => [
+        'http://localhost:5173',
+        'https://budgettrack-frontend-mt8l.onrender.com',
+    ],
+    'allowed_origins_patterns' => ['*'],
     'allowed_headers'          => ['*'],
     'exposed_headers'          => [],
     'max_age'                  => 0,
